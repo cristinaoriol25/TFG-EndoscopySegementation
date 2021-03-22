d5f8100bc5130cb0efe78cd464e41f3118af4d03
Cristina <oriol.cristina.25@gmail.com>
2021-03-22 12:27:39 +0100

    HOG

--- frame.py
+++ frame.py
@@ -1,5 +1,23 @@
+import cv2 
+
+
+
+
 class frame():
-    def __init__(self, video, nFrame):
-        self.video=video
+
+    hogStimate=None
+
+    def __init__(self, path, nFrame):
+        self.path=path
         self.numeroFrame=nFrame
-        #marcadores estadisticos de imagen. 
+        self.frame=cv2.imread(path+str(nFrame)+".png")
+        
+
+    def calcularHOG(self):
+        hog=cv2.HOGDescriptor()
+        self.hogStimate=hog.compute(self.frame)
+        print(self.hogStimate)
+        
+    def mostrarFrame(self):
+        cv2.imshow("Frame", self.frame)
+        cv2.waitKey(0)
